3c29e3b3e194b918341f8446697a17c13fac4147
FelipeOFF <foliveira@bancocbss.com.br>
2021-09-25 18:01:17 -0300

    AbstractGateway created to make it possible to make multiple clients, dependent on a host

---
+++ gateway/src/main/java/com/picpay/desafio/android/gateway/URLs.kt
@@ -0,0 +1,6 @@
+package com.picpay.desafio.android.gateway
+
+enum class URLs(val url: String) {
+    URL_PROD("https://609a908e0f5a13001721b74e.mockapi.io/picpay/api/"),
+    URL_MOCK("http://localhost:8080")
+}
