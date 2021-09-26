e85d0380b8e66f1482a649f1eaa9e20a7f1728c0
FelipeOFF <foliveira@bancocbss.com.br>
2021-09-26 18:05:24 -0300

    add test for message of error in screen

--- app/src/androidTest/java/com/picpay/desafio/android/activity/MainActivityTest.kt
+++ app/src/androidTest/java/com/picpay/desafio/android/activity/MainActivityTest.kt
@@ -1,14 +1,15 @@
-package com.picpay.desafio.android
+package com.picpay.desafio.android.activity
 
 import androidx.lifecycle.Lifecycle
 import androidx.test.core.app.launchActivity
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.hasDescendant
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.platform.app.InstrumentationRegistry
-import com.picpay.desafio.android.RecyclerViewMatchers.checkRecyclerViewItem
-import com.picpay.desafio.android.activity.MainActivity
+import com.picpay.desafio.android.R
+import com.picpay.desafio.android.adapter.RecyclerViewMatchers.checkRecyclerViewItem
 import com.picpay.desafio.android.gateway.mock.PicPayMockWebServer
 import com.picpay.desafio.android.gateway.mock.PicPayMockWebServer.MOCK_SERVER_PORT
 import okhttp3.mockwebserver.Dispatcher
@@ -24,16 +25,7 @@ class MainActivityTest {
     private val context = InstrumentationRegistry.getInstrumentation().targetContext
 
     private val mockServer: MockWebServer by lazy {
-        MockWebServer().apply {
-            dispatcher = object : Dispatcher() {
-                override fun dispatch(request: RecordedRequest): MockResponse {
-                    return when (request.path) {
-                        "/users" -> PicPayMockWebServer.successUserResponse
-                        else -> PicPayMockWebServer.errorUserResponse
-                    }
-                }
-            }
-        }
+        MockWebServer()
     }
 
     @Before
@@ -48,6 +40,7 @@ class MainActivityTest {
 
     @Test
     fun shouldDisplayTitle() {
+        mockServer.dispatcher = getAllDispatcher(PicPayMockWebServer.successUserResponse)
         launchActivity<MainActivity>().apply {
             val expectedTitle = context.getString(R.string.title)
 
@@ -59,10 +52,30 @@ class MainActivityTest {
 
     @Test
     fun shouldDisplayListItem() {
+        mockServer.dispatcher = getAllDispatcher(PicPayMockWebServer.successUserResponse)
         launchActivity<MainActivity>().apply {
             Thread.sleep(1000)
             checkRecyclerViewItem(R.id.recyclerView, 0, withText("@eduardo.santos"))
             checkRecyclerViewItem(R.id.recyclerView, 0, withText("Eduardo Santos"))
+        }
+    }
+
+    @Test
+    fun sholdDisplayErrorMessage() {
+        mockServer.dispatcher = getAllDispatcher(PicPayMockWebServer.errorUserUnauthorizedResponse)
+        launchActivity<MainActivity>().apply {
+            Thread.sleep(1000)
+            val errorUnknownAndServer = context.getString(R.string.error)
+            onView(hasDescendant(withText(errorUnknownAndServer)))
+        }
+    }
+
+    private fun getAllDispatcher(usersMockResponse: MockResponse) = object : Dispatcher() {
+        override fun dispatch(request: RecordedRequest): MockResponse {
+            return when (request.path) {
+                "/users" -> usersMockResponse
+                else -> PicPayMockWebServer.errorUserResponse
+            }
         }
     }
 }
--- app/src/androidTest/java/com/picpay/desafio/android/gateway/mock/PicPayMockWebServer.kt
+++ app/src/androidTest/java/com/picpay/desafio/android/gateway/mock/PicPayMockWebServer.kt
@@ -1,19 +1,40 @@
 package com.picpay.desafio.android.gateway.mock
 
 import okhttp3.mockwebserver.MockResponse
+import java.net.HttpURLConnection
 
 object PicPayMockWebServer {
 
     val successUserResponse by lazy {
-        val body =
-            "[{\"id\":1001,\"name\":\"Eduardo Santos\",\"img\":\"https://randomuser.me/api/portraits/men/9.jpg\",\"username\":\"@eduardo.santos\"}]"
+        val body = """
+            [
+                {
+                    "id": 1001,
+                    "name": "Eduardo Santos",
+                    "img": "https://randomuser.me/api/portraits/men/9.jpg",
+                    "username": "@eduardo.santos"
+                }
+            ]
+        """.trimIndent()
 
         MockResponse()
-            .setResponseCode(200)
+            .setResponseCode(HttpURLConnection.HTTP_OK)
             .setBody(body)
     }
 
-    val errorUserResponse by lazy { MockResponse().setResponseCode(404) }
+    val errorUserUnauthorizedResponse by lazy {
+        val body = """
+            {
+                "message": "Acesso negado"
+            }
+        """.trimIndent()
+
+        MockResponse()
+            .setResponseCode(HttpURLConnection.HTTP_UNAUTHORIZED)
+            .setBody(body)
+    }
+
+    val errorUserResponse by lazy { MockResponse().setResponseCode(HttpURLConnection.HTTP_NOT_FOUND) }
 
     const val MOCK_SERVER_PORT = 8080
 }
