0b04267bf7c29b8d8be894b60b8327653e313d1d
FelipeOFF <foliveira@bancocbss.com.br>
2021-09-26 14:39:48 -0300

    get user by domain layer

--- app/src/main/java/com/picpay/desafio/android/MainActivity.kt
+++ app/src/main/java/com/picpay/desafio/android/MainActivity.kt
@@ -6,49 +6,74 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.picpay.desafio.android.gateway.service.PicPayService
-import com.picpay.desafio.android.model.users.res.User
+import com.picpay.desafio.android.domain.ResultWrapper
+import com.picpay.desafio.android.domain.user.GetUserUseCase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.conflate
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import kotlin.coroutines.CoroutineContext
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
+class MainActivity : AppCompatActivity(R.layout.activity_main), CoroutineScope {
+
+    private val job = Job()
+    override val coroutineContext: CoroutineContext
+        get() = Main + job
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var progressBar: ProgressBar
     private lateinit var adapter: UserListAdapter
 
-    private val service: PicPayService by inject()
+    private val userUseCase: GetUserUseCase by inject()
 
     override fun onResume() {
         super.onResume()
+        setupView()
+        executeCall()
+    }
 
+    private fun setupView() {
         recyclerView = findViewById(R.id.recyclerView)
         progressBar = findViewById(R.id.user_list_progress_bar)
 
         adapter = UserListAdapter()
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
+    }
 
-        progressBar.visibility = View.VISIBLE
-        service.getUsers()
-            .enqueue(object : Callback<List<User>> {
-                override fun onFailure(call: Call<List<User>>, t: Throwable) {
-                    val message = getString(R.string.error)
+    private fun executeCall() {
+        launch {
+            userUseCase(Unit).onEach { result ->
+                when (result) {
+                    is ResultWrapper.Loading -> {
+                        progressBar.visibility = View.VISIBLE
+                    }
+                    is ResultWrapper.DismissLoading -> {
+                        progressBar.visibility = View.GONE
+                    }
+                    is ResultWrapper.Success -> {
+                        progressBar.visibility = View.GONE
 
-                    progressBar.visibility = View.GONE
-                    recyclerView.visibility = View.GONE
+                        adapter.users = result.value
+                    }
+                    is ResultWrapper.Error -> {
+                        val message = getString(R.string.error)
 
-                    Toast.makeText(this@MainActivity, message, Toast.LENGTH_SHORT)
-                        .show()
+                        progressBar.visibility = View.GONE
+                        recyclerView.visibility = View.GONE
+
+                        Toast.makeText(this@MainActivity, message, Toast.LENGTH_SHORT)
+                            .show()
+                    }
                 }
-
-                override fun onResponse(call: Call<List<User>>, response: Response<List<User>>) {
-                    progressBar.visibility = View.GONE
-
-                    adapter.users = response.body()!!
-                }
-            })
+            }.launchIn(CoroutineScope(coroutineContext))
+        }
     }
 }
