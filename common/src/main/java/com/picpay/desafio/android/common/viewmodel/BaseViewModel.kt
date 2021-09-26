5826b36a9af05e3c8b99c22a7db4b32fd6e7dd33
FelipeOFF <foliveira@bancocbss.com.br>
2021-09-26 16:06:44 -0300

    create BaseViewModel.kt for all viewmodels in project

---
+++ common/src/main/java/com/picpay/desafio/android/common/viewmodel/BaseViewModel.kt
@@ -0,0 +1,13 @@
+package com.picpay.desafio.android.common.viewmodel
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.Job
+import kotlin.coroutines.CoroutineContext
+
+abstract class BaseViewModel : ViewModel(), CoroutineScope {
+    private val job = Job()
+    override val coroutineContext: CoroutineContext
+        get() = Main + job
+}
